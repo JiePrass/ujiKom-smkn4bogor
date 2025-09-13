361d8c9aa1711710594deccdf1e269f47eae0373
JiePrass <renjieprass@gmail.com>
2025-09-13 20:33:34 +0700

    add font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,23 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local"
 import "./globals.css";
 import { AuthProvider } from "@/context/authContext";
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
+const sfPro = localFont({
+  src: [
+    { path: "../../public/fonts/SF-Pro-Display-Ultralight.otf", weight: "200", style: "normal" },
+    { path: "../../public/fonts/SF-Pro-Display-Thin.otf", weight: "300", style: "normal" },
+    { path: "../../public/fonts/SF-Pro-Display-Light.otf", weight: "300", style: "normal" },
+    { path: "../../public/fonts/SF-Pro-Display-Regular.otf", weight: "400", style: "normal" },
+    { path: "../../public/fonts/SF-Pro-Display-Medium.otf", weight: "500", style: "normal" },
+    { path: "../../public/fonts/SF-Pro-Display-Semibold.otf", weight: "600", style: "normal" },
+    { path: "../../public/fonts/SF-Pro-Display-Bold.otf", weight: "700", style: "normal" },
+    { path: "../../public/fonts/SF-Pro-Display-Heavy.otf", weight: "800", style: "normal" },
+    { path: "../../public/fonts/SF-Pro-Display-Black.otf", weight: "900", style: "normal" },
+  ],
+  variable: "--font-sfpro",
+  display: "swap",
+})
 
 export const metadata: Metadata = {
   title: "SIMKAS - Sistem Informasi Manajemen Kegiatan Akademi Sekolah",
@@ -17,7 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <AuthProvider>
-        <body className={`${inter.variable} antialiased`}>
+        <body className={`${sfPro.variable} antialiased`}>
           {children}
         </body>
       </AuthProvider>
